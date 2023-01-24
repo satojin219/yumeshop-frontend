2eb9b2fb22de90a1b042fd317d86592d6797c811
satojin219 <renzuoteng142@gmail.com>
2023-01-24 10:42:11 +0900

    chore: _app.tsxにviewportを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import GlobalStyle from 'src/styles/Globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
