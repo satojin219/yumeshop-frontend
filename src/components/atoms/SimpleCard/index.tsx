8906856ebd718aaa44aa627d963bbc499552b1a7
satojin219 <renzuoteng142@gmail.com>
2023-01-24 10:35:28 +0900

    style: cardの最大幅をfigmaと合わせる

--- src/components/atoms/SimpleCard/index.tsx
+++ src/components/atoms/SimpleCard/index.tsx
@@ -13,7 +13,7 @@ const Wrapper = styled.div`
   margin: 16px 4px;
   background: #ffffff;
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
-  max-width: 300px;
+  max-width: 244px;
   min-width: 200px;
   overflow: hidden;
   border-radius: 8px;
@@ -23,7 +23,7 @@ const ImageWrapper = styled.div`
   aspect-ratio: 16/9;
 `;
 const TitleWrapper = styled.p`
-  padding: 12px;
+  padding: 12px 8px;
 `;
 
 export const SimpleCard: VFC<SimpleCardProps> = (props) => {
