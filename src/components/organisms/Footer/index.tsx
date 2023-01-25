ff9d7c099ef06c615bb355d65fdff6203672a750
satojin219 <renzuoteng142@gmail.com>
2023-01-25 13:06:31 +0900

    feat: footerコンポーネントを作成

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -10,10 +10,44 @@ const Wrapper = styled.footer`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+  background: #ffffff;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+`;
+const LinkWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 8px 20px;
+  margin-bottom: 64px;
+`;
+const Link = styled.a`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 17px;
+  color: #000000;
+  text-decoration: underline
 `;
 
-export const Footer: VFC = () => (
-  <Wrapper>
-    <p>Copyright {new Date().getFullYear()} YumemiShop</p>
-  </Wrapper>
-);
+interface LinkType {
+  name: string;
+  path: string;
+}
+interface FooterProps {
+  links: LinkType[];
+}
+
+export const Footer: VFC<FooterProps> = (props) => {
+  const { links } = props;
+  return (
+    <Wrapper>
+      <LinkWrapper>
+        {links.map((link: LinkType) => (
+          <Link href={link.path} key={link.path}>{link.name}</Link>
+          
+        ))}
+      </LinkWrapper>
+      <p>Copyright {new Date().getFullYear()} YumemiShop</p>
+    </Wrapper>
+  );
+};
