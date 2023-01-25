eadcf5d36ae1e58c9d943cc8881bca0330266de9
satojin219 <renzuoteng142@gmail.com>
2023-01-25 14:42:30 +0900

    feat: WideCardコンポーネントを作成

---
+++ src/components/organisms/WideCardSection/index.tsx
@@ -0,0 +1,34 @@
+import { VFC } from 'react';
+import { Headline } from 'src/components/atoms/Headline';
+import { WideCard, WideCardProps } from 'src/components/atoms/WideCard';
+import styled from 'styled-components';
+
+export interface WideCardSectionProps {
+  heading: string;
+  wideCardArray: WideCardProps[];
+}
+
+const WideCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-top: 16px;
+`;
+
+export const WideCardSection: VFC<WideCardSectionProps> = (props) => {
+  const { heading, wideCardArray } = props;
+  return (
+    <section >
+      <Headline label={heading} headlineTypes="middle" />
+      <WideCardWrapper>
+        {wideCardArray.map((simpleCardItem: WideCardProps) => (
+          <WideCard
+            time={simpleCardItem.time}
+            labels={simpleCardItem.labels}
+            text={simpleCardItem.text}
+          />
+        ))}
+      </WideCardWrapper>
+    </section>
+  );
+};
